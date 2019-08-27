356d7a82213c4698c6f7972896e778904a6dbd71
EDSON LUIZ CANDIDO <edson.luiz.candido@gmail.com>
2019-08-26 21:40:03 -0300

    Commit inicial

---
+++ CsvToDataTable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace CsvToDataTable
+{
+    public class CsvToDataTable
+    {
+        DataTable dataTable = new DataTable();
+        private string _csvPath
+        {
+            get
+            {
+
+                return _csvPath;
+            }
+            set
+            {
+                _csvPath = value;
+            }
+        }
+        public CsvToDataTable(string csvPath)
+        {
+
+        }
+    }
+}
