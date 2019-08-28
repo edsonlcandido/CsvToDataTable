4103b44f62abb9a6fd58aec994af3918f056d171
EDSON LUIZ CANDIDO <edson.luiz.candido@gmail.com>
2019-08-27 23:28:07 -0300

    add a csv sample file "IECMotorFrameSize.csv"

--- CsvToDataTable.cs
+++ CsvToDataTable.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
+using System.IO;
 
 namespace CsvToDataTable
 {
@@ -14,7 +15,6 @@ namespace CsvToDataTable
         {
             get
             {
-
                 return _csvPath;
             }
             set
@@ -22,9 +22,38 @@ namespace CsvToDataTable
                 _csvPath = value;
             }
         }
+
+        private StreamReader _sr
+        {
+            get
+            {
+                return new StreamReader(_csvPath);
+            }
+        }
+
+        public CsvToDataTable()
+        {
+        }
+
         public CsvToDataTable(string csvPath)
         {
+            this._csvPath = csvPath;            
+        }
 
+        private void streamReaderToDataTable()
+        {
+            string[] headers = _sr.ReadLine().Split(';');
+            foreach (string header in headers)
+            {
+                dataTable.Columns.Add(header);
+            }
+        }
+
+        public DataTable dataTableExample()
+        {
+            this._csvPath = "IECMotorFrameSize.csv";
+            streamReaderToDataTable();
+            return dataTable;
         }
     }
 }
